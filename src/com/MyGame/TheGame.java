4ecc26a921827c1e0bb90ccd04a7e2bb15a2363e
Abayomi <yommy.sonaike@gmail.com>
2023-06-27 22:27:17 +0100

    Initial commit

---
+++ src/com/MyGame/TheGame.java
@@ -0,0 +1,71 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class TheGame implements ActionListener, MouseListener, KeyListener {
+
+    public static TheGame theGame;
+    public final  int WIDTH = 1200, HEIGHT = 700;
+    public Renderer renderer;
+    public Rectangle player;
+    public ArrayList<Rectangle> walls;
+    public int ticks, yvelocity, score;
+    public boolean gameOver = false, started = false;
+    public Random random;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
